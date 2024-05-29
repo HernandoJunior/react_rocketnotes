59ba6e09d81bddfe9607739786c54e557876a605
= <devhernandojunior@gmail.com>
2024-05-29 16:17:24 -0300

    add styles files and button

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,11 +1,14 @@
 import { Container } from "./style.js"
+import { Button } from "../../components/Button/index.jsx"
 
 export function Details(){
-
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Hernando Junior</span>
+      <h1>Hernando Junior</h1>
+      <span>ReactJS</span>
+      <Button title= "Entrar" />
+      <Button title= "Cadastrar" />
+      <Button title= "Voltar" />
     </Container>
   )
 }
--- src/pages/details/style.js
+++ src/pages/details/style.js
@@ -1,12 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  background: red;
-  display: flex;
-  align-items: center;
-  gap: 10px;
+export const Container = styled.div `
+
 `
 
-// body {
-//   background-color: aliceblue;
-// }
