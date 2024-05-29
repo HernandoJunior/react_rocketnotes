8aa5f3ad6df68bc1078fd6dac8fa6193cdc463af
= <devhernandojunior@gmail.com>
2024-05-29 14:35:25 -0300

    add styled components

---
+++ src/pages/details/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./style.js"
+
+export function Details(){
+
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+      <span>Hernando Junior</span>
+    </Container>
+  )
+}
+
---
+++ src/pages/details/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: red;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`
+
+// body {
+//   background-color: aliceblue;
+// }
